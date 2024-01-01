f30211ff37f66da06c5271bb064e043458c340d8
Joelle  Johnson <joellej221@gmail.com>
2024-01-01 13:00:45 -0500

    Add Projects component to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import About from './components/About.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
+import Projects from './components/Projects.jsx';
 
 function App() {
   return (
     <main className='bg-slate-800 h-screen'>
     <Header />
     <About />
+    <Projects />
     <Footer />
     </main>
   );
---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+export default function Projects(){
+    return(
+        <section id="projects" className="flex flex-col justify-center items-center text-slate-50">
+            <h2 className="text-3xl font-bold">Projects</h2>
+            <p className="text-xl">Here are some of my projects:</p>
+            <ul>
+                <li><a href="https://final-project-630f3.web.app">WishLily</a>
+                <p className="text-xl">WishLily is a full-stack application that allows users to create and share wishlists with friends and family. It was built using React, Next.js, Express, PostGreSQL, Node, Firebase, and Tailwind CSS.</p>
+                </li>
+            </ul>
+        </section>
+    )
+}
