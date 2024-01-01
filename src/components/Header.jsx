3931d37ce236336bc8853e8af6f2a45674394ff5
Joelle  Johnson <joellej221@gmail.com>
2024-01-01 12:47:28 -0500

    Update dependencies and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 export default function Header() {
     return (
         <header>
-        <h1>Joelle Johnson</h1>
+        <h1 className="text-red-800">Joelle Johnson</h1>
+        <nav>
+            <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
         </header>
     );
     }
