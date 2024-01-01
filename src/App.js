e0e08b376f59a2015669564a17100a29a908e0de
Joelle  Johnson <joellej221@gmail.com>
2024-01-01 13:05:16 -0500

    Add Contact component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import Projects from './components/Projects.jsx';
@@ -10,6 +11,7 @@ function App() {
     <Header />
     <About />
     <Projects />
+    <Contact />
     <Footer />
     </main>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact(){
+    return(
+        <section id="contact" className="flex flex-col justify-center items-center text-slate-50">
+            <h2 className="text-3xl font-bold">Contact</h2>
+            <p className="text-xl">You can email me at <a href="mailto:joellej221@gmail.com>">JoelleJ221@gmail.com</a></p>
+            <p className="text-xl">Add me on <a href="https://www.linkedin.com/in/joelle-c-johnson/">LinkedIn</a></p>
+            <p className="text-xl">Check out my <a href="https://github.com/JoelleCJohnson">Github</a></p>
+        </section>
+    )
+}
