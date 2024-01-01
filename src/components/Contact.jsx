45928da0c48daa05746b2c4506cf70f9ae48382e
Joelle  Johnson <joellej221@gmail.com>
2024-01-01 13:07:22 -0500

    Update contact information in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,9 @@ export default function Contact(){
     return(
         <section id="contact" className="flex flex-col justify-center items-center text-slate-50">
             <h2 className="text-3xl font-bold">Contact</h2>
-            <p className="text-xl">You can email me at <a href="mailto:joellej221@gmail.com>">JoelleJ221@gmail.com</a></p>
-            <p className="text-xl">Add me on <a href="https://www.linkedin.com/in/joelle-c-johnson/">LinkedIn</a></p>
-            <p className="text-xl">Check out my <a href="https://github.com/JoelleCJohnson">Github</a></p>
+            <p className="text-xl">You can email me at <a className="underline" href="mailto:joellej221@gmail.com>">JoelleJ221@gmail.com</a></p>
+            <p className="text-xl">Add me on <a className="underline" href="https://www.linkedin.com/in/joelle-c-johnson/">LinkedIn</a>.</p>
+            <p className="text-xl">Check out my <a className="underline" href="https://github.com/JoelleCJohnson">Github</a>.</p>
         </section>
     )
 }
