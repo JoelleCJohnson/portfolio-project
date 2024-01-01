be492305e181dc61e42386093185fbf000af46a9
Joelle  Johnson <joellej221@gmail.com>
2024-01-01 13:27:45 -0500

    Update Contact and Projects components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact(){
     return(
-        <section id="contact" className="flex flex-col justify-center items-center text-slate-50">
+        <section id="contact" className="flex flex-col justify-center items-center text-slate-50 bg-slate-600 shadow">
             <h2 className="text-3xl font-bold">Contact</h2>
             <p className="text-xl">You can email me at <a className="underline" href="mailto:joellej221@gmail.com>">JoelleJ221@gmail.com</a></p>
             <p className="text-xl">Add me on <a className="underline" href="https://www.linkedin.com/in/joelle-c-johnson/">LinkedIn</a>.</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,8 @@ export default function Projects(){
         <section id="projects" className="flex flex-col justify-center items-center text-slate-50">
             <h2 className="text-3xl font-bold">Projects</h2>
             <p className="text-xl">Here are some of my projects:</p>
-            <ul>
-                <li><a href="https://final-project-630f3.web.app">WishLily</a>
+            <ul className="flex flex-col items-center w-15/16">
+                <li className="text-center"><a className="text-xl underline" href="https://final-project-630f3.web.app">WishLily</a>
                 <p className="text-xl">WishLily is a full-stack application that allows users to create and share wishlists with friends and family. It was built using React, Next.js, Express, PostGreSQL, Node, Firebase, and Tailwind CSS.</p>
                 </li>
             </ul>
